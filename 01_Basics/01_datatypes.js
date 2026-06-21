46f24b3b44f51b80167a9cbb40abd04de828bdfd
vaibhav-jain-05 <vaibhavraajjain5@gmail.com>
2026-06-21 14:21:53 +0000

    added variable and dataTypes

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3+3); //we are using nodejs not browser
+
+console.log(3+
+    3);  //not write code like this code should be readable weather the code is right but it should be readable
+
+//DataTyps
+//====Primitive=======
+//number ==> 2 to poer of 53
+//bigint
+//string ==> ""
+//boolean ==> true or false
+//null ==> standalone value ===> object
+//undefined => never assiagned value ===>undefined
+//symbol ==> uinique
+
+
+//object
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+
+const accountID = 23445555
+let   accountEmail = "xyz@hmail.com"
+var   accountPass = "123@4445"
+
+accountCity = "Jaipur"
+
+let accountState;
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPass,accountCity,accountState]);
+
+/*
+prefer const and let
+never use var because of block and functional scope issue
+*/
