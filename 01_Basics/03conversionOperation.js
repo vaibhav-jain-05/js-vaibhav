6aadd3c215bf28048b97d28795dfa76c68bf7722
vaibhav-jain-05 <vaibhavraajjain5@gmail.com>
2026-06-21 15:01:26 +0000

    conversionOperation2

--- 01_Basics/03conversionOperation.js
+++ 01_Basics/03conversionOperation.js
@@ -7,24 +7,49 @@ let score = "33"
 // console.log(typeof valueInNumber)
 // console.log(typeof (valueInNumber))
 
-//"33" ==> 33
-//"33abc" ==> NaN
-//true   ===>1 ; false ==>0
+// //"33" ==> 33
+// //"33abc" ==> NaN
+// //true   ===>1 ; false ==>0
 
-let isLoggedIn = 100
+// let isLoggedIn = 100
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(typeof booleanIsLoggedIn)
-// console.log(booleanIsLoggedIn)
-
-
-//"" ==> false
-//" " or " any string value" ==> true
-//any number == true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(typeof booleanIsLoggedIn)
+// // console.log(booleanIsLoggedIn)
 
 
-let someNumber = 233;
+// //"" ==> false
+// //" " or " any string value" ==> true
+// //any number == true
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+
+// let someNumber = 233;
+
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+
+
+//******************************Operations******************************************
+
+//console.log(2+2);  sum
+//console.log(2-2); 
+//console.log(2*2);
+//console.log(2**2);  ==> 2^2 power of 2
+//console.log(2/2); 
+//console.log(2%3);   get reminder
+
+
+
+let str1 = "vaibhav"
+let str2 = "Jain"
+
+let str3 = str1 + str2 
+console.log(str3)
+
+console.log("1" + 2)  //12
+console.log("1" + "2")  //12
+console.log("1" + 2 +2)  //122
+console.log(2+2+3+"9")   //79 
