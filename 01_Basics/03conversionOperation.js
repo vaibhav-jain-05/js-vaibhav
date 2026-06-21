83d0fd9b210fb81feb55883ca7d749a7e520278a
vaibhav-jain-05 <vaibhavraajjain5@gmail.com>
2026-06-21 14:46:22 +0000

    conversionOperation

---
+++ 01_Basics/03conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(typeof (valueInNumber))
+
+//"33" ==> 33
+//"33abc" ==> NaN
+//true   ===>1 ; false ==>0
+
+let isLoggedIn = 100
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+//"" ==> false
+//" " or " any string value" ==> true
+//any number == true
+
+
+let someNumber = 233;
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
