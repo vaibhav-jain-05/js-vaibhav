225948f7f9bb700e8e1364c75cf78a38e23ed0bf
vaibhav-jain-05 <vaibhavraajjain5@gmail.com>
2026-06-22 08:31:16 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+
+//basic camparison same dataType No issue 
+console.log(2>5)   //false
+console.log(2<5)  //true
+console.log(2==5) //false
+console.log(2!=0)  //true
+
+//Above line giving right camparison
+//==============================================================================================
+
+//conversion and Comparisn two dataTypes
+console.log("2" > 1);  //true actual ......>>>>>>>>> string 2 > number 1
+console.log("02" > 1); //true
+
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined > 0);  //false
+console.log(undefined == 0); //false
+console.log(undefined < 0); //false
+
+//=== works in camarison
+console.log("2"  === 2)   //false
+
+
+
